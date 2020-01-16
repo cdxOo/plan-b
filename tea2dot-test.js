a6cd262d68cbec91a2ba4ca1d39c19d16c4f1216
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-01-16 11:29:46 +0100

    dot-notation condition label is now inside of the node instead of the leading edge

--- tea2dot-test.js
+++ tea2dot-test.js
@@ -1,5 +1,9 @@
 var tea2dot = require('./tea2dot'),
-    recipe = require('./something-graph');
+    fspath = require('path');
+
+var args = process.argv.slice(2);
+
+var recipe = require(fspath.resolve(args[0]));
 
 var s = (
     tea2dot
