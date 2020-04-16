fc79f0c96e0944a1b9522dd813b116d1ba497546
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-04-16 12:33:24 +0200

    moved tea-mug2.js to tea-mug; added state maniuplation to stub test file

--- src/tea-mug.spec.js
+++ src/tea-mug.spec.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect,
-    TeaMug = require('./tea-mug2');
+    TeaMug = require('./tea-mug');
 
 var recipes = [
     {
@@ -54,15 +54,28 @@ var recipes = [
 
 describe('TeaMug()', () => {
     it('things', () => {
+        var water = 0,
+            temp = 0;
         var barista = (
             TeaMug()
             .recipes(...recipes)
             .onAction((node) => {
                 console.log(node.key);
+                if (node.name === 'more water') {
+                    water += 0.1;
+                }
+                else if (node.key === '/boil water/wait') {
+                    temp += 10;
+                }
             })
             .onCondition((node) => {
-                //console.log(node.key);
-                return true;
+                console.log(node.key);
+                if (node.name === 'enough water in boiler?') {
+                    return (water > 0.4)
+                }
+                else if (node.name === 'water hot?') {
+                    return (temp >= 100)
+                }
             })
         );
 
